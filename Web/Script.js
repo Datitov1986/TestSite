fe907589cc29c6bc66a60f704768aa1ee0434b31
Datitov1986 <datitov2009@yandex.ru>
2023-06-27 22:43:24 +0300

    Добавил условный оператор в скрипт

--- Web/Script.js
+++ Web/Script.js
@@ -2,3 +2,8 @@ let number = 7;
 const numbers = 18;
 number = 10;
 console.log(number + numbers);
+let boll = true;
+if (boll) {
+ console.log("Если условие истино, то этот текст выводится на экран");
+ console.log("Если условие будет ложным, то текст будет пропущен");
+}
